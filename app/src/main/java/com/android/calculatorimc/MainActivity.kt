d81d69825d037a61627e69b3e9ce93c2a1caafc9
Nicolás Cabezas <126811829+nicolascabezas1@users.noreply.github.com>
2023-09-15 21:50:52 -0500

    :zap: Improve performance::gender CardView color change When you click on a gender card, the colors and states change to selected and unselected respectively.

--- app/src/main/java/com/android/calculatorimc/MainActivity.kt
+++ app/src/main/java/com/android/calculatorimc/MainActivity.kt
@@ -1,17 +1,52 @@
 package com.android.calculatorimc
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var cardViewMale: CardView
+    private lateinit var cardViewFemale: CardView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initComponents()
+        initListeners()
+    }
+
+    private fun initComponents() {
+        cardViewMale = findViewById(R.id.cardViewMale)
+        cardViewFemale = findViewById(R.id.cardViewFemale)
+    }
+
+    private fun initListeners() {
+        cardViewMale.setOnClickListener {
+            setCardViewSelected(cardViewMale)
+            setCardViewUnselected(cardViewFemale)
+        }
+        cardViewFemale.setOnClickListener {
+            setCardViewSelected(cardViewFemale)
+            setCardViewUnselected(cardViewMale)
+        }
+    }
+
+    private fun setCardViewSelected(cardView: CardView) {
+        cardView.setCardBackgroundColor(
+            ContextCompat.getColor(
+                this,
+                R.color.imc_color_component_selected
+            )
+        )
+    }
+
+    private fun setCardViewUnselected(cardView: CardView) {
+        cardView.setCardBackgroundColor(
+            ContextCompat.getColor(
+                this,
+                R.color.imc_color_component
+            )
+        )
     }
 }
