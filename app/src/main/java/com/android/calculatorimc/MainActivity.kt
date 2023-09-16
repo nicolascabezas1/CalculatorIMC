935fc2f85feb96613e4df91105cddf011530c7bb
Nicolás Cabezas <cabezasn628@gmail.com>
2023-09-15 21:49:29 -0500

    :sparkles: [Introduce new feature] :: completed construction of the main UX The UX has a selection of gender, height, weight and age

---
+++ app/src/main/java/com/android/calculatorimc/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.android.calculatorimc
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
