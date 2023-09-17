c9e58426cad1cee0421e6cb26addb9676f60153d
Nicolás Cabezas <cabezasn628@gmail.com>
2023-09-17 15:00:44 -0500

    :zap: [Improve performance] :: finished height module The numerical values are updated and the value is saved in an integer variable, with a global scope.
    by Nicolás Cabezas

--- app/src/main/java/com/android/calculatorimc/MainActivity.kt
+++ app/src/main/java/com/android/calculatorimc/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.android.calculatorimc
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
+import com.google.android.material.slider.RangeSlider
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
+    private var currentHeight: Int = 120
     private lateinit var cardViewMale: CardView
     private lateinit var cardViewFemale: CardView
-
+    private lateinit var textViewHeight: TextView
+    private lateinit var rangeSliderHeight: RangeSlider
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,8 +25,11 @@ class MainActivity : AppCompatActivity() {
     private fun initComponents() {
         cardViewMale = findViewById(R.id.cardViewMale)
         cardViewFemale = findViewById(R.id.cardViewFemale)
+        textViewHeight = findViewById(R.id.textViewHeight)
+        rangeSliderHeight = findViewById(R.id.rangeSliderHeight)
     }
 
+    @SuppressLint("SetTextI18n")
     private fun initListeners() {
         cardViewMale.setOnClickListener {
             setCardViewSelected(cardViewMale)
@@ -29,6 +38,11 @@ class MainActivity : AppCompatActivity() {
         cardViewFemale.setOnClickListener {
             setCardViewSelected(cardViewFemale)
             setCardViewUnselected(cardViewMale)
+        }
+        rangeSliderHeight.addOnChangeListener { _, value, _ ->
+            val decimalFormat = DecimalFormat("#.##")
+            val currentHeight = decimalFormat.format(value).toInt()
+            textViewHeight.text ="$currentHeight cm"
         }
     }
 
