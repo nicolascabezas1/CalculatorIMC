2c9c94b90d9552d6ce41c37fd8eb341ea0e7c585
Nicolás Cabezas <cabezasn628@gmail.com>
2023-09-17 16:08:04 -0500

    :sparkles: [New feature] :: IMC calculation completed By pressing the "Calculate BMI" button you can navigate to another activity and the calculation is done through the console.
    by Nicolás Cabezas

--- app/src/main/java/com/android/calculatorimc/MainActivity.kt
+++ app/src/main/java/com/android/calculatorimc/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.android.calculatorimc
 
-import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.slider.RangeSlider
 import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
     private var currentHeight: Int = 120
+    private var currentWeight: Int = 60
+    private var currentAge: Int = 20
+    private val decimalFormat = DecimalFormat("#.##")
+
     private lateinit var cardViewMale: CardView
     private lateinit var cardViewFemale: CardView
     private lateinit var textViewHeight: TextView
     private lateinit var rangeSliderHeight: RangeSlider
+    private lateinit var textViewWeight: TextView
+    private lateinit var buttonRemoveWeight: FloatingActionButton
+    private lateinit var buttonAddWeight: FloatingActionButton
+    private lateinit var textViewAge: TextView
+    private lateinit var buttonRemoveAge: FloatingActionButton
+    private lateinit var buttonAddAge: FloatingActionButton
+    private lateinit var buttonCalculate: Button
+
+    companion object {
+        const val IMC_KEY = "IMC_RESULT"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,9 +44,15 @@ class MainActivity : AppCompatActivity() {
         cardViewFemale = findViewById(R.id.cardViewFemale)
         textViewHeight = findViewById(R.id.textViewHeight)
         rangeSliderHeight = findViewById(R.id.rangeSliderHeight)
+        textViewWeight = findViewById(R.id.textViewWeight)
+        buttonAddWeight = findViewById(R.id.buttonAddWeight)
+        buttonRemoveWeight = findViewById(R.id.buttonRemoveWeight)
+        textViewAge = findViewById(R.id.textViewAge)
+        buttonRemoveAge = findViewById(R.id.buttonRemoveAge)
+        buttonAddAge = findViewById(R.id.buttonAddAge)
+        buttonCalculate = findViewById(R.id.buttonCalculate)
     }
 
-    @SuppressLint("SetTextI18n")
     private fun initListeners() {
         cardViewMale.setOnClickListener {
             setCardViewSelected(cardViewMale)
@@ -40,17 +63,35 @@ class MainActivity : AppCompatActivity() {
             setCardViewUnselected(cardViewMale)
         }
         rangeSliderHeight.addOnChangeListener { _, value, _ ->
-            val decimalFormat = DecimalFormat("#.##")
             val currentHeight = decimalFormat.format(value).toInt()
-            textViewHeight.text ="$currentHeight cm"
+            textViewHeight.text = "$currentHeight cm"
+        }
+        buttonRemoveWeight.setOnClickListener {
+            currentWeight--
+            showText(textViewWeight, currentWeight, "kg")
+        }
+        buttonAddWeight.setOnClickListener {
+            currentWeight++
+            showText(textViewWeight, currentWeight, "kg")
+        }
+        buttonAddAge.setOnClickListener {
+            currentAge++
+            showText(textViewAge, currentAge, "")
+        }
+        buttonRemoveAge.setOnClickListener {
+            currentAge--
+            showText(textViewAge, currentAge, "")
+        }
+        buttonCalculate.setOnClickListener {
+            val imc: Double = calculateIMC()
+            navigateToResult(imc)
         }
     }
 
     private fun setCardViewSelected(cardView: CardView) {
         cardView.setCardBackgroundColor(
             ContextCompat.getColor(
-                this,
-                R.color.imc_color_component_selected
+                this, R.color.imc_color_component_selected
             )
         )
     }
@@ -58,9 +99,32 @@ class MainActivity : AppCompatActivity() {
     private fun setCardViewUnselected(cardView: CardView) {
         cardView.setCardBackgroundColor(
             ContextCompat.getColor(
-                this,
-                R.color.imc_color_component
+                this, R.color.imc_color_component
             )
         )
     }
+
+    private fun showText(textView: TextView, value: Int, units: String) {
+        val textToShow = "$value $units"
+        textView.text = textToShow
+    }
+
+    private fun calculateIMC(): Double {
+        return try {
+            val heightInMeters = currentHeight.toDouble() / 100
+            val imc: Double = currentWeight / (heightInMeters * heightInMeters)
+            decimalFormat.format(imc).toDouble()
+        } catch (e: ArithmeticException) {
+            0.0
+        } catch (e: NumberFormatException) {
+            0.0
+        }
+    }
+
+    private fun navigateToResult(imc: Double) {
+        val intent = Intent(this, ResultActivity::class.java)
+        intent.putExtra("IMC_RESULT", imc)
+        startActivity(intent)
+    }
+
 }
